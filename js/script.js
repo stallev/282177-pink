b18c28459cc6727aade6f02cb4ad56202effc2b9
stallev <stwork87@gmail.com>
2017-03-29 13:40:53 -0700

    added jscript menu opening-closing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 "use strict";
 
 (function () {
-
+  var hamburger = document.querySelector('.header__toggle-hamburger');
+  var cross = document.querySelector('.header__toggle-cross');
+  var nav = document.querySelector('.nav');
+  
+  hamburger.addEventListener('click', onDisplayNav);
+  cross.addEventListener('click', onCloseNav);
+  
+  function onDisplayNav() {
+    nav.style.display = "block";
+    hamburger.style.display = "none";
+    cross.style.display = "block";
+  }
+  function onCloseNav() {
+    nav.style.display = "none";
+    hamburger.style.display = "block";
+    cross.style.display = "none";
+  }
 })();
