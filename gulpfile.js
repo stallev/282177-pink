a31647c59a327b1a1b564907dc4e77329cb907db
stallev <stwork87@gmail.com>
2017-04-06 00:47:45 +0300

    add deployment action

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var imagemin = require('gulp-imagemin');
 var run = require('run-sequence');
 var del = require('del');
 var rename = require('gulp-rename');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('style', function() {
   gulp.src('sass/style.scss')
@@ -70,6 +71,11 @@ gulp.task('images', function() {
     imagemin.jpegtran({progressive: true})
     ]))
   .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('deploy', function () {
+  return gulp.src('build/**/*')
+    .pipe(ghPages());
 });
 
 gulp.task('build', function(fn){
